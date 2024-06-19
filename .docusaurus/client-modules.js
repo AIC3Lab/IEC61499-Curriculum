999296812d9c14ff12cf8cfe96c7b86561e525ec
midhunxavier <midhunxavier@outlook.com>
2024-06-19 17:43:19 +0200

    Initial tutorial commit

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("C:\\Users\\midxav\\Documents\\SApps\\IEC61499-Curriculum\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("C:\\Users\\midxav\\Documents\\SApps\\IEC61499-Curriculum\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("C:\\Users\\midxav\\Documents\\SApps\\IEC61499-Curriculum\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
+  require("C:\\Users\\midxav\\Documents\\SApps\\IEC61499-Curriculum\\src\\css\\custom.css"),
+];
---
+++ docs/Module1/presentation.md
@@ -0,0 +1,3 @@
+# Presentation
+
+[See the PowerPoint presentation](https://docs.google.com/presentation/d/1tK1wgnZ8k7FHD0FCNvEtbJwJojd3vOsp)
